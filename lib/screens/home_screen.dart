ef97b2e1d4c84eea23d21839294834e5558dca05
kharioki <antolisah@gmail.com>
2020-06-14 01:14:53 +0300

    search box

--- lib/screens/home_screen.dart
+++ lib/screens/home_screen.dart
@@ -11,6 +11,73 @@ class _HomeScreenState extends State<HomeScreen> {
   Widget build(BuildContext context) {
     return Scaffold(
       backgroundColor: white,
+      body: SafeArea(
+        child: ListView(
+          padding: EdgeInsets.symmetric(horizontal: 12),
+          children: [
+            Row(
+              mainAxisAlignment: MainAxisAlignment.spaceBetween,
+              children: [
+                Text(
+                  'What would you like to eat?',
+                  style: TextStyle(
+                    fontSize: 18,
+                  ),
+                ),
+                Stack(
+                  children: [
+                    IconButton(
+                      icon: Icon(Icons.notifications_none),
+                      onPressed: () {},
+                    ),
+                    Positioned(
+                      top: 12,
+                      right: 12,
+                      child: Container(
+                        height: 10,
+                        width: 10,
+                        decoration: BoxDecoration(
+                          color: red,
+                          borderRadius: BorderRadius.circular(20),
+                        ),
+                      ),
+                    ),
+                  ],
+                ),
+              ],
+            ),
+            SizedBox(height: 10),
+            Container(
+              decoration: BoxDecoration(
+                color: white,
+                boxShadow: [
+                  BoxShadow(
+                    color: grey[300],
+                    offset: Offset(1, 1),
+                    blurRadius: 4,
+                  ),
+                ],
+              ),
+              child: ListTile(
+                leading: Icon(
+                  Icons.search,
+                  color: red,
+                ),
+                title: TextField(
+                  decoration: InputDecoration(
+                    hintText: 'Find food and restaurant',
+                    border: InputBorder.none,
+                  ),
+                ),
+                trailing: Icon(
+                  Icons.filter_list,
+                  color: red,
+                ),
+              ),
+            ),
+          ],
+        ),
+      ),
     );
   }
 }
